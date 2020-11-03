1ddeaf91f5189936bd365f1fd61d0adcb1fc6be5
ridoan.anik <ridoankhan07@gmail.com>
2020-11-03 07:39:31 +0600

    All updated

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -5,22 +5,30 @@ const {
     getAllBootcamps,
     getSingleBootcamp,
     getBootcampByName,
+    getBootcampsInRadius,
     createBootcamp,
     updateBootcamp,
     deleteBootcamp,
-    deleteBootcampByName
+    deleteBootcampByName,
+    deleteAllBootcamps
 } = require('../Controllers/bootcamps');
 
 router.route('/')
-    .get(getAllBootcamps)
-    .post(createBootcamp);
+    .get(getAllBootcamps)   // Get list of all bootcamps
+    .post(createBootcamp)   // Create a new bootcamp   
+    .delete(deleteAllBootcamps) // Delete all bootcamps
+
+router.route('/radius/:zipcode/:distance')
+    .get(getBootcampsInRadius)  // Get bootcamps within given distance of a zipCode
 
 router.route('/:id')
-    .get(getSingleBootcamp)
-    .put(updateBootcamp)
-    .delete(deleteBootcamp);
+    .get(getSingleBootcamp) // Get a single bootcamp with bootcamp id
+    .put(updateBootcamp)    // Update a bootcamp info with id and data
+    .delete(deleteBootcamp) // Delete a bootcamp with an id
 
-router.route('/name/:name').get(getBootcampByName).delete(deleteBootcampByName);
+router.route('/name/:name')
+    .get(getBootcampByName) // Delete a bootcamp by bootcamp-name
+    .delete(deleteBootcampByName)   // Delete list of all bootcamps
 
 
 module.exports = router;
