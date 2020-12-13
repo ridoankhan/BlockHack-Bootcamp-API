d1406fa54b59a2205a7788da80f2c4515b97192b
ridoankhan <ridoankhan07@gmail.com>
2020-12-13 19:26:11 +0600

    deleted all

--- middleware/logger.js
+++
@@ -1,9 +0,0 @@
-// @desc    A logger for all requests to the server
-const logger = (req, res, next) => {
-    console.log(
-        `${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`
-    );
-    next();
-};
-
-module.exports = logger;
